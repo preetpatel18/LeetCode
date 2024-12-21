edadb2322199a26478061a00aa276e8ba269c69a
Preet Patel <preetpatel1862@gmail.com>
2024-12-21 18:37:42 -0500

    Faster O(n)

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -1,5 +1,7 @@
 package Easy;
 
+import java.util.HashMap;
+
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0; i< nums.length-1; i++){
@@ -7,6 +9,21 @@ class TwoSum {
                 if(nums[i]+nums[j] == target){
                     return new int[]{i, j};
                 }
+            }
+        }
+        return null;
+    }
+
+    // Faster Algorithm: Run time of O(n)
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int difference =target - nums[i];
+            int remainder = target - difference;
+            if(hm.containsKey((Integer)remainder)){
+                return new int[]{hm.get(remainder), i};
+            }else{
+                hm.put(difference, i);
             }
         }
         return null;
