b798d80f64bb2d32f451f1dbb13c7ea0d2dd1b0c
Preet Patel <preetpatel1862@gmail.com>
2025-05-02 00:13:01 -0400

    NeetCode

---
+++ Medium/LongestConsecutiveSequence.java
@@ -0,0 +1,36 @@
+package Medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    class Solution {
+        public int longestConsecutive(int[] nums) {
+            int max = 0;
+            Set<Integer> set = new HashSet<Integer>();
+            for (int i = 0; i < nums.length; i++) {
+                set.add(nums[i]);
+            }
+
+            for(int i=0; i < nums.length; i ++ ){
+                int count = 1;
+                int num = nums[i];
+                
+                while(set.contains(--num)){
+                    count++;
+                    set.remove(num);
+                }
+
+                num = nums[i];
+                while(set.contains(++num)){
+                    count++;
+                    set.remove(num);
+                }
+
+                max = Math.max(max, count);
+            }
+
+            return max;
+        }
+    }
+}
